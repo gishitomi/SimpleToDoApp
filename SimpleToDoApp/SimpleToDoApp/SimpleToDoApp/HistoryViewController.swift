5a8d05cb038f106a7f36757398b1834d72cd16a0
宜志富紹太 <shotagishitomi@gmail.com>
2023-03-21 18:29:20 +0900

    カレンダーライブラリFSCalendar追加 履歴画面にカレンダー画面追加

--- SimpleToDoApp/SimpleToDoApp/SimpleToDoApp/HistoryViewController.swift
+++ SimpleToDoApp/SimpleToDoApp/SimpleToDoApp/HistoryViewController.swift
@@ -6,10 +6,24 @@
 //
 
 import UIKit
+import FSCalendar
 
 class HistoryViewController: UIViewController {
 
+    @IBOutlet weak var calendar: FSCalendar!
     override func viewDidLoad() {
+        // calendarの曜日部分を日本語表記に変更
+        calendar.calendarWeekdayView.weekdayLabels[0].text = "日"
+        calendar.calendarWeekdayView.weekdayLabels[1].text = "月"
+        calendar.calendarWeekdayView.weekdayLabels[2].text = "火"
+        calendar.calendarWeekdayView.weekdayLabels[3].text = "水"
+        calendar.calendarWeekdayView.weekdayLabels[4].text = "木"
+        calendar.calendarWeekdayView.weekdayLabels[5].text = "金"
+        calendar.calendarWeekdayView.weekdayLabels[6].text = "土"
+        // calendarの曜日部分の色を変更
+        calendar.calendarWeekdayView.weekdayLabels[0].textColor = .systemRed
+        calendar.calendarWeekdayView.weekdayLabels[6].textColor = .systemBlue
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
--- SimpleToDoApp/SimpleToDoApp/SimpleToDoApp/ViewController.swift
+++ SimpleToDoApp/SimpleToDoApp/SimpleToDoApp/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var TableView: UITableView!
+    
+    let samples = ["ぎしこ", "紹太"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return samples.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //セル取得
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+        
+        //セルに表示する値を決める
+        cell.textLabel!.text = samples[indexPath.row]
+        return cell
+    }
+    
+    
 
 }
 
